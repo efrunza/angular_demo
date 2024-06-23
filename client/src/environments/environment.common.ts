b633dca677dba8d6b4d6da51ca35ebbc707f7ef1
Eugen Frunza <efrunza@hotmail.com>
2024-06-23 09:09:46 -0400

    Update environment.common.ts

--- client/src/environments/environment.common.ts
+++ client/src/environments/environment.common.ts
@@ -2,7 +2,7 @@ const applicantSignInPolicy = 'B2C_1_signupsignin';
 const agentSignInPolicy = 'B2C_1_agentsignupsignin';
 const resetPasswordPolicy = 'B2C_1_resetpassword';
 const azureInstance = `https://login.microsoftonline.com/tfp`;
-const azureInstance_v2 = `https://IAPDEVB2C.b2clogin.com/tfp`;
+const azureInstance_v2 = `https://.b2clogin.com/tfp`;
 const azureApplicantCallbackUrl = 'application';
 const azureAgentCallbackUrl = 'agent';
 
